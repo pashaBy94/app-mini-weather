8a7eb2e5af32dbcfebb0f0bf36485b94d36eeaad
pashaBy94 <pascom_by@mail.ru>
2024-09-13 22:19:50 +0300

    any

--- src/entities/sunTime/SunTime.tsx
+++ src/entities/sunTime/SunTime.tsx
@@ -1,31 +1,33 @@
-import { iconMarker } from '@/lib/images';
-import Image from 'next/image';
+import { parseData, parseUTC } from '@/shared/lib';
 import { useEffect } from 'react';
+import { setSunny } from './lib/utils';
 
 export function SunTime({ sunrise, sunset, dt }: { sunrise: number; sunset: number; dt: number }) {
     useEffect(() => {
-        console.log(sunset - sunrise, sunset - dt);
-
-        //     const collection = document.querySelectorAll(
-        //         '.rotate__deg',
-        //     ) as NodeListOf<HTMLImageElement>;
-        //     collection.forEach(el => {
-        //         el.style.rotate = `${180 + (deg as number)}deg`;
-        //     });
+        setSunny({
+            sunrise,
+            sunset,
+            dt,
+            node: document.querySelector('.fanny_suny') as HTMLImageElement,
+        });
     }, []);
     return (
         <div className="bg-white rounded-xl p-2 flex flex-col gap-2">
             <h3 className=" text-[.8rem]">Закат и рассвет</h3>
-            <div className=" relative flex gap-2">
+            <div className=" relative flex gap-2 items-center">
                 <p className=" font-semibold">
-                    <span className=" text-gray-600 text-[1.1rem]">{sunrise}</span> м/с
+                    <span className=" text-gray-600 text-[1.1rem]">
+                        {parseData(parseUTC(sunrise), ['time'])}
+                    </span>{' '}
                 </p>
-                <div className=" relative h-8 w-28 overflow-hidden">
-                    <div className=" absolute z-10 rounded-full w-4 h-4 bg-yellow-500 bottom-0 left-0"></div>
-                    <div className=" absolute top-0 left-0 rounded-[50%] w-28 h-16 border-dotted border-gray-600 border-[1px]"></div>
+                <div className=" relative h-10 w-28 overflow-hidden ">
+                    <div className=" fanny_suny absolute z-10 rounded-full w-4 h-4 bg-yellow-500 "></div>
+                    <div className=" absolute top-3 left-2 rounded-[50%] w-24 h-16 border-dotted border-gray-600 border-[1px]"></div>
                 </div>
                 <p className=" font-semibold">
-                    <span className=" text-gray-600 text-[1.1rem]">{sunset}</span> м/с
+                    <span className=" text-gray-600 text-[1.1rem]">
+                        {parseData(parseUTC(sunset), ['time'])}
+                    </span>{' '}
                 </p>
             </div>
         </div>
---
+++ src/entities/sunTime/lib/utils.ts
@@ -0,0 +1,21 @@
+export function setSunny({ sunset, sunrise, dt, node }: { [key: string]: any }) {
+    function timePassedtoPercent({ sunset, sunrise, dt, node }: { [key: string]: any }): number {
+        const delta = sunset - sunrise;
+        const onePercent = delta / 100;
+        const howLong = dt - sunrise;
+        const howLongPercent = Math.round(howLong / onePercent);
+        return howLongPercent > 100 ? 100 : howLongPercent;
+    }
+    const perc = timePassedtoPercent({ sunset, sunrise, dt, node });
+    const deltaBottom = perc <= 50 ? perc * 2 : (100 - perc) * 2;
+    if (perc < 25 || perc > 75) {
+        node.style.bottom = `${deltaBottom}%`;
+    } else {
+        node.style.top = `${100 - perc * 2}%`;
+    }
+    if (perc > 50) {
+        node.style.right = `${100 - perc}%`;
+    } else {
+        node.style.left = `${perc}%`;
+    }
+}

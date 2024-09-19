acc7999c5a21bd22c6430a9f9912b3e5f907476c
pashay94 <pascom_by@mail.ru>
2024-09-20 01:30:03 +0300

    change

--- src/widgets/newsList/NewsList.tsx
+++ src/widgets/newsList/NewsList.tsx
@@ -5,7 +5,7 @@ import { OnlyNewsData } from '@/shared/model/types';
 export function NewsList({ newsData }: { newsData: Array<OnlyNewsData> | undefined }) {
     return (
         <section className="mb-5">
-            <h2 className="mb-2 ">Только свежие новости</h2>
+            <h2 className="mb-2 ">Последние новости</h2>
             <ul className=" grid am:grid-cols-1 cm:grid-cols-2 gap-4">
                 {newsData &&
                     newsData.map(item => {

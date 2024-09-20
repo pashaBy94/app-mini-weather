14b74c82a166f46b6d24f789dc994145332e89dc
pashay94 <pascom_by@mail.ru>
2024-09-20 07:10:45 +0300

    animation

--- src/entities/hourlyItem/HourlyItem.tsx
+++ src/entities/hourlyItem/HourlyItem.tsx
@@ -8,7 +8,16 @@ export function HourlyItem({ hour, ind }: HourlyItemComponent) {
         const collection = document.querySelectorAll(
             '.rotate__deg',
         ) as NodeListOf<HTMLImageElement>;
-        collection.forEach(el => {
+        const collectionCloud = document.querySelectorAll(
+            '.cloud__bounce',
+        ) as NodeListOf<HTMLImageElement>;
+        collectionCloud.forEach((el, ind) => {
+            el.style.transition = `all ${Math.min(3.5, ind / 2)}s .1s cubic-bezier(0.2,-2,0.8,2)`;
+            el.style.transform = `translate(0, -4px)`;
+        });
+        collection.forEach((el, ind) => {
+            el.style.rotate = `0deg`;
+            el.style.transition = `all ${Math.min(3.5, ind / 2)}s .05s cubic-bezier(.30, .10, 1, 0)`;
             el.style.rotate = `${180 + (hour.deg as number)}deg`;
         });
     }, []);
@@ -16,7 +25,13 @@ export function HourlyItem({ hour, ind }: HourlyItemComponent) {
     return (
         <li className=" flex flex-col items-center gap-3">
             <p className="font-bold text-[.9em]">{Math.round(+hour.temp)}&deg;</p>
-            <Image src={hour.clouds + ''} alt="clouds" width={30} height={30} />
+            <Image
+                src={hour.clouds + ''}
+                alt="clouds"
+                width={30}
+                height={30}
+                className="cloud__bounce block"
+            />
             <p className="text-[0.7rem] font-semibold">{ind === 0 ? 'Сейчас' : hour.time}</p>
             <div className="flex flex-col items-center gap-1">
                 <Image
